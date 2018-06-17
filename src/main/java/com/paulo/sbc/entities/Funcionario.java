639cfa1ed068d70f9128a950b3c201ce4c5ddd73
pauloemmilio <paulo.emilio@dce.ufpb.br>
2018-06-17 10:56:26 -0300

    criação da configuração inicial e das entidades iniciais do projeto

---
+++ src/main/java/com/paulo/sbc/entities/Funcionario.java
@@ -0,0 +1,215 @@
+package com.paulo.sbc.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+import com.paulo.sbc.enums.PerfilEnum;
+
+@Entity
+@Table(name = "funcionario")
+public class Funcionario implements Serializable{
+
+	private static final long serialVersionUID = -8021881043394662978L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "nome", nullable = false)
+	private String nome;
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+	
+	@Column(name = "senha", nullable = false)
+	private String senha;
+	
+	@Column(name = "cpf", nullable = false)
+	private String cpf;
+	
+	@Column(name = "valor_hora", nullable = true)
+	private BigDecimal valorHora;
+	
+	@Column(name = "qtd_horas_trabalho_dia", nullable = true)
+	private Float qtdHorasTrabalhadas;
+	
+	@Column(name = "qtd_horas_almoco", nullable = true)
+	private Float qtdHorasAlmoco;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "perfil", nullable = false)
+	private PerfilEnum perfil;
+	
+	@Column(name = "data_criacao", nullable = false)
+	private Date dataCriacao;
+	
+	@Column(name = "data_atualizacao", nullable = false)
+	private Date dataAtualizacao;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Empresa empresa;
+	
+	@OneToMany(mappedBy = "funcionario", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Lancamento> lancamentos;
+	
+	public Funcionario() {}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public BigDecimal getValorHora() {
+		return valorHora;
+	}
+	
+	public Optional<BigDecimal> getValorHoraOptional(){
+		return Optional.ofNullable(valorHora);
+	}
+
+	public void setValorHora(BigDecimal valorHora) {
+		this.valorHora = valorHora;
+	}
+
+	public Float getQtdHorasTrabalhadas() {
+		return qtdHorasTrabalhadas;
+	}
+	
+	public Optional<Float> getQtdHorasTrabalhadasOptional(){
+		return Optional.ofNullable(qtdHorasTrabalhadas);
+	}
+
+	public void setQtdHorasTrabalhadas(Float qtdHorasTrabalhadas) {
+		this.qtdHorasTrabalhadas = qtdHorasTrabalhadas;
+	}
+
+	public Float getQtdHorasAlmoco() {
+		return qtdHorasAlmoco;
+	}
+	
+	public Optional<Float> getQtdHorasAlmocoOptional(){
+		return Optional.ofNullable(qtdHorasAlmoco);
+	}
+
+	public void setQtdHorasAlmoco(Float qtdHorasAlmoco) {
+		this.qtdHorasAlmoco = qtdHorasAlmoco;
+	}
+
+	public PerfilEnum getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(PerfilEnum perfil) {
+		this.perfil = perfil;
+	}
+
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+
+	public List<Lancamento> getLancamentos() {
+		return lancamentos;
+	}
+
+	public void setLancamentos(List<Lancamento> lancamentos) {
+		this.lancamentos = lancamentos;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		this.dataAtualizacao = new Date();
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		final Date atual = new Date();
+		this.dataCriacao = atual;
+		this.dataAtualizacao = atual;
+	}
+
+	@Override
+	public String toString() {
+		return "Funcionario [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + ", cpf=" + cpf
+				+ ", valorHora=" + valorHora + ", qtdHorasTrabalhadas=" + qtdHorasTrabalhadas + ", qtdHorasAlmoco="
+				+ qtdHorasAlmoco + ", perfil=" + perfil + ", dataCriacao=" + dataCriacao + ", dataAtualizacao="
+				+ dataAtualizacao + ", empresa=" + empresa + "]";
+	}
+	
+}
---
+++ src/main/java/com/paulo/sbc/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.paulo.sbc.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
